e638d995eed00d23b64bd53cf6cc4a177836cc18
Sebastien Ramsay <ramsa0130@algonquinlive.com>
2023-07-01 08:27:49 -0400

    s

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -27,7 +27,7 @@ const Login = () =>{
                 {/* {error && <div className="error">{error}</div>} */}
             </form>
             <p class="p-5 font-bold text-xl">or</p>
-            <Link to="https://ramsays-detailing-frontend.onrender.com/auth/google">
+            <Link to="https://ramsays-detailing.onrender.com/auth/google">
                 <button class="bg-blue-600 flex items-center">
                     <img src='https://ramsays-detailing.onrender.com/images/google.png' alt="google" class="h-auto w-10 p-1"/>
                     <span class="mx-2 text-gray-50">Sign in with Google</span>
